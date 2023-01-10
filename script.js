6784a192c04303e61ed5edf7ba10211c1c11acd4
Timothy Njoroge <timothynjoroge56@gmail.com>
2023-01-10 22:29:02 +0300

    Add data to local storage after submission Render data from local storage Lint files

--- script.js
+++ script.js
@@ -1,30 +1,55 @@
-document.getElementById("form").addEventListener('submit', function(q) {
-    q.preventDefault()
+let bookArray = [];
+const booksList = document.getElementById('books_list');
 
-    console.log("test")
-    let auther = document.getElementById('auther_input').value
-    let title = document.getElementById("title_input").value
-    console.log (auther, title)
+function saveToLocalStorage(data) {
+  localStorage.setItem('awesome_books', JSON.stringify(data));
+}
 
-    const data = {title, auther}
-    book_array.push(data)
-    console.log(book_array)
+function renderBooks(data) {
+  booksList.innerHTML = '';
+  data.forEach((book) => {
+    const li = document.createElement('li');
+    li.style.borderBottom = 'solid';
+    li.style.borderWidth = '1px';
+    li.style.paddingBottom = '10px';
+    const autherTitle = document.createElement('p');
+    autherTitle.innerHTML = `${book.title} <br> ${book.auther}`;
 
-    let div = document.createElement("div")
-    div.style.borderBottom = "solid"
-    div.style.borderWidth = "1px"
-    div.style.paddingBottom = "5px"
-    let auther_title = document.createElement('p')
-    auther_title.innerHTML = title + "<br>" + auther
-    
-    let del_button = document.createElement("button")
-    del_button.innerHTML = "delete"
-    div.appendChild(auther_title)
-    div.appendChild(del_button)
-    del_button.addEventListener('click', function() {
-        div.remove()
-    })
-    document.getElementById('elements').appendChild(div)
-})
+    const delButton = document.createElement('button');
+    delButton.innerHTML = 'Remove';
+    li.appendChild(autherTitle);
+    li.appendChild(delButton);
+    delButton.addEventListener('click', () => {
+      li.remove();
+      bookArray = bookArray.filter((item) => !(JSON.stringify(item) === JSON.stringify(book)));
+      saveToLocalStorage(bookArray);
+      renderBooks(bookArray);
+    });
 
-let book_array = []
+    booksList.appendChild(li);
+  });
+}
+
+document.getElementById('form').addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const auther = document.getElementById('auther_input').value;
+  const title = document.getElementById('title_input').value;
+
+  const data = { title, auther };
+  bookArray.push(data);
+
+  renderBooks(bookArray);
+  saveToLocalStorage(bookArray);
+  e.target.reset();
+});
+
+function loadLocalStorageData() {
+  const localStorageData = JSON.parse(localStorage.getItem('awesome_books'));
+  bookArray = localStorageData.length ? localStorageData : [];
+}
+
+window.addEventListener('load', () => {
+  loadLocalStorageData();
+  renderBooks(bookArray);
+});
